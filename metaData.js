8512ccb77a309e7ff9fab546c0ddc020d6fccb87
Himanshu Dixit <hudixt@gmail.com>
2021-04-14 08:44:53 +0530

    Add meta

--- metaData.js
+++ metaData.js
@@ -1,3 +1,20 @@
-export const CONFIG ={
-  NAME: "Himanshu Dixit"
+export default {
+  homepage:{
+      tags:[],
+      articles: [{
+          name: '',
+          url: ''
+      }]
+  },
+  email: "hello@himanshudixit.me",
+  socialMediaURLs : {
+      twitter: "https://twitter.com/1x_engineer",
+      github: "https://github.com/himanshu-dixit",
+      instagram: "https://instagram.com/1xengineer",
+      music: "https://twitter.com/1x_engineer",
+      books: ""
+  },
+  emailCaptureURL:{
+
+  }
 }
