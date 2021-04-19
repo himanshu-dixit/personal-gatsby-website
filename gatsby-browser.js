b190da58be030659be982b8b8bbf4ceb4ea72f93
Himanshu Dixit <hudixt@gmail.com>
2021-04-19 18:09:45 +0530

    Cleanup code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,3 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-// normalize CSS across browsers
-// import "./src/styling/normalize.css"
-// // custom CSS styles¯
 import "./src/styling/fonts.css"
 import "./src/styling/style.css"
-//
-// // Highlighting for code blocks
-// import "prismjs/themes/prism.css"
