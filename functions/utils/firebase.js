a669bffb31e78328736202dc733009465048c1c7
Himanshu Dixit <hudixt@gmail.com>
2021-06-06 06:32:25 +0530

    Remove firebase file

--- functions/utils/firebase.js
+++ functions/utils/firebase.js
@@ -1,10 +1,33 @@
 var admin = require("firebase-admin")
-var serviceAccount = require("../secrets/serviceAccountKey.json")
+const {
+  type,
+  project_id,
+  private_key_id,
+  private_key,
+  client_email,
+  client_id,
+  auth_uri,
+  token_uri,
+  auth_provider_x509_cert_url,
+  client_x509_cert_url} = process.env;
+
+const cert = type !== "undefined" ? JSON.stringify({
+  type,
+  project_id,
+  private_key_id,
+  private_key,
+  client_email,
+  client_id,
+  auth_uri,
+  token_uri,
+  auth_provider_x509_cert_url,
+  client_x509_cert_url}) : require("../secrets/serviceAccountKey.json")
+
 
 const initializeFirebase = () => {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(cert),
     })
   } catch (e) {
     console.log("Firebase already initialized")
